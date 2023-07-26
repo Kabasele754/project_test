72f5cecf609c29721203b4318480af55b199ec42
Achille kabasele <pepexykabasele@gmail.com>
2023-07-27 00:53:35 +0200

    update crud

--- app_blog/urls.py
+++ app_blog/urls.py
@@ -1,10 +1,14 @@
 from  django.urls import  path
-from .views import  indexView, contactView
+from .views import  indexView, contactView, gestionContactView,\
+    updateCotactView, deleteContactView
 
 
 urlpatterns = [
     path('', indexView, name="index"),
-    path('contact/', contactView, name='contact')
+    path('contact/', contactView, name='contact'),
+    path('gestion-contact/', gestionContactView, name="gestion_contact"),
+    path('gestion-contact/update/<int:contact_id>/', updateCotactView, name="update_contact"),
+    path('gestion-contact/delete/<int:contact_id>/', deleteContactView, name="delete_contact")
 
 ]
 
--- app_blog/views.py
+++ app_blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article, Contact
 
 # Create your views here.
@@ -24,10 +24,16 @@ def indexView(request, template_name="blog/pages/index.html"):
 
 
 def contactView(request, template_name="blog/pages/contact.html"):
-    print("je veux voir le request", request)
-    #  ici on verifie si la mathode de la request et == POST
+    context = {}
+    # SELECT * FROM Contact
+    contacts = Contact.objects.all()
+    # Utilisation du context
+    context["contacts"] = contacts
+
+    #  ici la condition on verifie si la mathode de la request et == POST
     if request.method == "POST":
         # On recupere les valeurs entrer par l'utilisateur
+
         name = request.POST['name']
         email = request.POST['email']
         content = request.POST['content']
@@ -42,8 +48,62 @@ def contactView(request, template_name="blog/pages/contact.html"):
         )
         obj_contact.save()
 
+    return render(request, template_name, context)
 
 
+def gestionContactView(request, template_name="blog/pages/gestion_contact.html"):
+    context = {}
+    # SELECT * FROM Contact
+    contacts = Contact.objects.all()
+    # Utilisation du context
+    context["contacts"] = contacts
+    #  ici la condition on verifie si la mathode de la request et == POST
+    if request.method == "POST":
+        # On recupere les valeurs entrer par l'utilisateur
+
+        name = request.POST['name']
+        email = request.POST['email']
+        content = request.POST['content']
+
+        print("je veux le nom", name)
+
+        #  On creer notre Contact
+        obj_contact = Contact(
+            name=name,
+            email=email,
+            content=content
+        )
+        obj_contact.save()
+
+    return render(request, template_name, context)
+
+# fonction  update
+
+def updateCotactView(request,contact_id, template_name="blog/pages/gestion_contact.html"):
+    contact_one = Contact.objects.get(id=contact_id)
+    print("id", contact_one)
+    context = {}
+    contacts = Contact.objects.all()
+    context['contacts'] = contacts
+    context['contact_one'] = contact_one
+    #  la condition == post
+    if request.method == "POST":
+        # recuper les donnees qui se trouve dans input et on le modifie
+        name = request.POST['name']
+        email = request.POST['email']
+        content = request.POST['content']
+        #  assign de valeurs input
+        contact_one.name = name
+        contact_one.email = email
+        contact_one.content = content
+        # on sauvegarde la modification
+        contact_one.save()
+
+    return render(request, template_name, context)
 
 
-    return render(request, template_name)
+def deleteContactView(request, contact_id):
+    contact_one = Contact.objects.get(id=contact_id)
+    contact_one.delete()
+
+    return redirect('gestion_contact')
